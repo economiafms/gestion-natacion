add4a8123ca92293b425d1bc8deed6b033202401
economiafms <economiafms@gmail.com>
2026-01-20 18:57:59 -0300

    Update 6_mi_categoria.py

--- pages/6_mi_categoria.py
+++ pages/6_mi_categoria.py
@@ -16,7 +16,7 @@ rol = st.session_state.role
 mi_id = st.session_state.user_id 
 mi_nombre = st.session_state.user_name
 
-st.title("🏆 Ranking por Categoría")
+st.title("Categorías")
 
 # --- CSS PERSONALIZADO ---
 st.markdown("""
